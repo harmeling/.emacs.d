9b048241dc5494fa859e8be7cfcf1deffc025196
Stefan Harmeling <harmeling@gmail.com>
2020-05-05 20:03:49 +0200

    moved deft notes to sciebo

--- init.el
+++ init.el
@@ -605,7 +605,7 @@
 ;; deft (notational velocity)
 (require 'deft)
 (setq deft-default-extension "org")
-(setq deft-directory "~/work/notes")
+(setq deft-directory "~/sciebo/notes")
 (setq deft-use-filename-as-title t)
 (setq deft-use-filter-string-for-filename t)
 (setq deft-auto-save-interval 100.0)
